5e42e35e4f6218b704025f755e84acdeefe3f655
Nadezhda Vasilyeva <nv702822@gmail.com>
2021-05-07 19:15:57 +0500

    Link

---
+++ core/src/com/project/Link.java
@@ -0,0 +1,65 @@
+package com.project;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+
+public class Link extends SimpleText implements Drawable {
+
+    String action;
+    boolean was_selected = false;
+    Font selected_font;
+
+    public Link (int x, int y, Font font, String text, String action) {
+        super(x, y, font, text);
+        this.action = action;
+        this.init();
+    }
+
+    public void init() {
+        float delta_color = -0.15f;
+        if (this.font.color == Color.BLACK) {
+            delta_color *= -1;
+        }
+        Color color = new Color(this.font.color.r + delta_color, this.font.color.g + delta_color, this.font.color.b + delta_color, 1.0f);
+        this.selected_font = new Font(this.font.location, this.font.size, color);
+        this.selected_font.init();
+    }
+
+    @Override
+    public void draw() {
+        this.processInput();
+        if (was_selected) {
+            this.selected_font.draw(this.x, this.y + Window.workspace.deltaY, this.text);
+        }
+        else {
+            this.font.draw(this.x, this.y + Window.workspace.deltaY, this.text);
+        }
+    }
+
+    private void processInput() {
+        if ((Gdx.input.getX() <= (this.x+this.width))
+              && (Gdx.input.getX() >= this.x)
+              && (Gdx.graphics.getHeight() -Gdx.input.getY() <=  this.y + Window.workspace.deltaY)
+              && (Gdx.graphics.getHeight() -Gdx.input.getY() >= ( this.y - this.height/2 + Window.workspace.deltaY))) {
+                if (!this.was_selected) {
+                    this.was_selected = true;
+                }
+                if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+                    if (this.action.startsWith("load=")) {
+                        Window.workspace.setWorkspace(this.action.substring(5));
+                    }
+                }
+        }
+        else {
+            if (this.was_selected) {
+                this.was_selected = false;
+            }
+        }
+    }
+
+    public void dispose() {
+        this.selected_font.dispose();
+    }
+
+}
